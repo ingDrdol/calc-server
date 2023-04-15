041a372edf810a864c38da6b6c41109d484a3989
xkocma09 <xkocma09@stud.fit.vutbr.cz>
2023-04-15 17:33:42 +0200

    feat: nested expressions

--- ipkcpd.c
+++ ipkcpd.c
@@ -26,6 +26,9 @@
 
 #define DEBUG
 #define BUF_LEN 255
+#define OP_POZ 1
+#define EXPR_OFFSET 3*sizeof(char)
+#define SP_OFFSET(x) x*sizeof(char)
 
 #define ERR_MSG(x) loc_error_msg[x*(-1) - 1]
 
@@ -47,7 +50,7 @@ double mul(double x, double y){
 double my_div(double x, double y){
     if(y == 0){
         errno = ERR_DIV;
-        return 0;
+        return -1;
     }
     return x / y;
 }
@@ -78,21 +81,65 @@ funcvar select_op(char operator){
  *
  * @returns computed value
 */
-double get_result(const char *prefix_eq){
-    double (*operation)(double, double);
-    double first_exp = prefix_eq[3] - 48, second_exp = prefix_eq[5] - 48; 
-    operation = select_op(prefix_eq[0]);
-    fprintf(stderr, "%c, %lf, %lf\n", prefix_eq[0], first_exp, second_exp);
+double get_result(const char *prefix_eq, char *rest){
+    int read_check, i;
+    char close;
+    double first_exp, second_exp = 5;
+    double (*operation)(double, double) = select_op(prefix_eq[OP_POZ]);
+    if(errno != 0)
+        return -1;
+
+    if(prefix_eq[0] != '('){
+        errno = ERR_OPENB;
+        return -1;
+    }
+
+///////////////////////////////////////////////////////////////////////////////////
+    read_check = sscanf(prefix_eq + EXPR_OFFSET, "%lf %[^'\n']", &first_exp, rest);
+    if(read_check != 2)
+        first_exp = get_result(prefix_eq + EXPR_OFFSET, rest);
+    if(errno != 0)
+        return -1;
+/////////////////////////////////^^first value^^///////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+    read_check = sscanf(rest, " %lf%[^\n]", &second_exp, rest);
+    if(read_check == 1){
+        errno = ERR_CLOSB;
+        return -1;
+    }
+    if(read_check != 2){
+        i = 0;
+        while(rest[i] != '(' && rest[i] != '\0')
+            i++;
+        if(rest[i] == '\0'){
+            errno = ERR_OPENB;
+            return -1;
+        }
+        second_exp = get_result(rest + SP_OFFSET(i), rest);
+    }
+    if(errno != 0)
+        return -1;  
+////////////////////////////////^^second value^^/////////////////////////////////////
+    printf("%s\n", rest);
+    read_check = sscanf(rest, "%c%[^\n]", &close, rest);
+    if(close != ')'){
+        errno = ERR_CLOSB;
+        return -1;
+    }
+    printf("%s, %c\n", rest, close);
     return operation(first_exp, second_exp);
 
 }
 
 int main(void){
     errno = 0;
+    char rest[BUF_LEN];
+    const char *test_string = {"(* (+ 1 (+ 1 1)) (- 10 (/ 10 2)"};
     double result;
-
-    result = get_result("(* 2 5)");
-    printf("%lf\n", result);
-
+    result = get_result(test_string, &rest[0]);
+    if(errno != 0)
+        fprintf(stderr, "%s\n", ERR_MSG(errno));
+    else
+        printf("%lf\n", result);
     return 0;
 }
